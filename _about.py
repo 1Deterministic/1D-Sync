fb0bbd82208482e2a4f6366c3a2eb00bf6b3c83d
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-07-20 12:04:39 -0300

    Version 2.0 finished, _about.py updated.

--- _about.py
+++ _about.py
@@ -1,6 +1,6 @@
 PROGRAM_NAME = "1D-Sync"
-PROGRAM_VERSION = "2.0-rc3"
-PROGRAM_BUILD_DATE = "2018/07/06"
+PROGRAM_VERSION = "2.0"
+PROGRAM_BUILD_DATE = "2018/07/20"
 
-EMAIL_SUBJECT = "1D-Sync Report Service"
+EMAIL_SUBJECT = "1D-Sync Alert Service"
 EMAIL_HEADER = "Last sync run status\n\n"
