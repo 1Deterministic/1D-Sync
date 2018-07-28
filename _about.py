8b9fd2e8e9062a9e6874ec42bf79668b4fc27949
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-07-28 18:35:36 -0300

    Forgot to update the about strings

--- _about.py
+++ _about.py
@@ -1,6 +1,6 @@
 PROGRAM_NAME = "1D-Sync"
-PROGRAM_VERSION = "2.1-alpha2"
-PROGRAM_BUILD_DATE = "2018/07/25"
+PROGRAM_VERSION = "2.1-alpha3"
+PROGRAM_BUILD_DATE = "2018/07/27"
 
 EMAIL_SUBJECT = "1D-Sync Alert Service"
 EMAIL_HEADER = "Last sync run status\n\n"
