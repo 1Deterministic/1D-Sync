a96a8ad3cdc6abdedbed0c31ee48bcd1a238aba9
Lucas Fernandes Vieira <lucas-fv@outlook.com>
2018-11-23 19:17:37 -0200

    Fixed the windows auto start script not working properly

--- windows-startup.bat
+++ windows-startup.bat
@@ -1,8 +1,8 @@
-# install the program to a folder in the user dir
 mkdir C:\Users\%username%\.1dsync
-xcopy * C:\Users\%username%\.1dsync
+robocopy . C:\Users\%username%\.1dsync /E
 
-# creates the init script file
+attrib +h C:\Users\%username%\.1dsync
+
 echo start pythonw C:\Users\%username%\.1dsync\1dsync.py > "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\1dsync.bat"
 
 echo Ok!
